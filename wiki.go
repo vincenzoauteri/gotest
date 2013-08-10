09ce572c23287b767f52807fb62a4d50ba61bf9e
Vincenzo Auteri <vincenzo.auteri@hotmail.com>
2013-08-11 00:31:45 +0200

    wiki

---
+++ wiki.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+        "fmt"
+        "io/ioutil"
+        "net/http"
+      )
+
+const lenPath = len("/view/")
+
+type Page struct {
+  Title string
+  Body []byte
+}
+
+func (p *Page) save() error {
+  filename := p.Title + ".txt"
+  return ioutil.WriteFile(filename,p.Body,0600)
+}
+
+func loadPage(title string) (*Page, error) {
+  filename := title + ".txt"
+  body, err := ioutil.ReadFile(filename)
+  if (err != nil) {
+    return nil, err
+  }
+  return &Page{Title:title,Body:body},nil
+}
+
+func viewHandler(w http.ResponseWriter, r  *http.Request) {
+  title:=r.URL.Parh[lenPath:]
+  p,_ :=loadPage(title)
+  fmt.Fprintf(w,"<h1>%s</h1><div>%s</div>",p.Title,p.Body)
+}
+
+
+func main () {
+  p1 := &Page{Title:"TestPage", Body:[]byte("This is a sample Page")}
+  p1.save()
+  p2,_ := loadPage("TestPage")
+  fmt.Println(string(p2.Body))
+}
